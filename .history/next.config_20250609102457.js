fcb71dc19d189aad28ae999868ccc289b16ebe7b
Abhishek Enaguthi <enaguthiabhishek@gmail.com>
2025-06-09 10:29:18 -0700

    Deploy static site to root for GitHub Pages user site

---
+++ .history/next.config_20250609102457.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
+  },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/personalwebsite' : '',
+}
+
+module.exports = nextConfig 
