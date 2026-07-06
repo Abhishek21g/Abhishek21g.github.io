b2d92442bf3711481572d8b8ea433dfb85462f59
Abhishek21g <enaguthiabhishek@gmail.com>
2026-07-06 01:36:12 +0000

    Deploying to gh-pages from @ Abhishek21g/Abhishek21g.github.io@d5210920e1d346eb94d6eed918e4d39ba4499adb 🚀

--- tinker-workbench/site/script.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector(".site-header");
-
-const updateHeader = () => {
-  if (!header) return;
-  header.dataset.scrolled = window.scrollY > 10 ? "true" : "false";
-};
-
-window.addEventListener("scroll", updateHeader, { passive: true });
-updateHeader();
